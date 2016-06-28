cb196083c8e3a9ff56bf40c871d19eae9e1b8eb6
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-06-28 14:40:46 -0300

    Renomeado nome do pacote do app para "com.rasfalto"

--- app/src/main/java/com/rasfalto/LoginActivity.java
+++ app/src/main/java/com/rasfalto/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.cintia.rasfalto;
+package com.rasfalto;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
--- app/src/main/java/com/rasfalto/MainActivity.java
+++ app/src/main/java/com/rasfalto/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.cintia.rasfalto;
+package com.rasfalto;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
