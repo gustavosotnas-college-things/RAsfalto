758901eba0d1cd95e07498ff0e6888d4a949680e
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-06-30 17:05:47 -0300

    Renomeada `MainActivity` para `LoginActivity` e `LoginActivity` para `RAsfaltoActivity`.

--- app/src/main/java/com/rasfalto/LoginActivity.java
+++ app/src/main/java/com/rasfalto/LoginActivity.java
@@ -1,13 +1,21 @@
 package com.rasfalto;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity
-{
+public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view){
+
+        Intent loginActivity = new Intent(this, RAsfaltoActivity.class);
+        startActivity(loginActivity);
     }
 }
