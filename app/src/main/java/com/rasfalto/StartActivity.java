b23d4279bf1109c22b6a24834459aff3e4e5c875
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-07-02 21:28:17 -0300

    Criada tela inicial de apresentação do RAsfalto.

---
+++ app/src/main/java/com/rasfalto/StartActivity.java
@@ -0,0 +1,21 @@
+package com.rasfalto;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void onClick$Login(View view)
+    {
+        Intent loginActivity = new Intent(this, LoginActivity.class);
+        startActivity(loginActivity);
+    }
+}
