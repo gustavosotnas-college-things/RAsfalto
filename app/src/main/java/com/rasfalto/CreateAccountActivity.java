2a9d0c71f3cb8a530a44960a44cd6d958e2d44b4
RenanOfugi <renangyn2010@hotmail.com>
2016-07-07 16:36:05 -0400

    atualização da tentativa de login

--- app/src/main/java/com/rasfalto/CreateAccountActivity.java
+++ app/src/main/java/com/rasfalto/CreateAccountActivity.java
@@ -1,12 +1,19 @@
 package com.rasfalto;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Map;
 
@@ -15,7 +22,7 @@ public class CreateAccountActivity extends AppCompatActivity {
     private EditText emailinsert;
     private EditText passwordinsert;
 
-    Firebase ref = new Firebase("https://rasfalto-d70cd.firebaseio.com/");
+    private FirebaseAuth mAuth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,33 +30,54 @@ public class CreateAccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
 
-        emailinsert = (EditText) findViewById(R.id.emailcadastro);
-        passwordinsert = (EditText) findViewById(R.id.senhacadastro);
-
         Firebase.setAndroidContext(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    public void RealizarCadastro(View view){
+    public void RealizarCadastro(View view) {
 
-        String email = emailinsert.getText().toString();
-        String password = passwordinsert.getText().toString();
+        setEmailinsert((EditText) findViewById(R.id.emailcadastro));
+        setPasswordinsert((EditText) findViewById(R.id.senhacadastro));
 
-        Create(email,password);
+        System.out.println(getEmailinsert());
+        System.out.println(getPasswordinsert());
+
+        String email = getEmailinsert().getText().toString();
+        String password = getPasswordinsert().getText().toString();
+
+        Create(email, password);
     }
 
-    private void Create(String email, String password){
+    private void Create(String email, String password) {
 
-        ref.createUser(email, password, new Firebase.ValueResultHandler<Map<String, Object>>(){
-
+        Firebase ref = new Firebase("https://Rasfalto.firebaseio.com");
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onSuccess(Map<String, Object> result) {
-                System.out.println("Conta criada com Sucesso");
-            }
+            public void onComplete(@NonNull Task<AuthResult> task) {
 
-            @Override
-            public void onError(FirebaseError firebaseError) {
-                System.out.println("Erro ao criar a conta");
+                Log.d("login","Autenticação realizada com sucesso");
+
+                if(!task.isSuccessful()){
+                    Log.w("login", "onComplete: ", task.getException());
+                }
             }
         });
+    }
+
+    public EditText getEmailinsert() {
+        return emailinsert;
+    }
+
+    public void setEmailinsert(EditText emailinsert) {
+        this.emailinsert = emailinsert;
+    }
+
+    public EditText getPasswordinsert() {
+        return passwordinsert;
+    }
+
+    public void setPasswordinsert(EditText passwordinsert) {
+        this.passwordinsert = passwordinsert;
     }
 }
