43e6bd241b31e370e4b544f48369a08e9397f92e
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-07-08 20:55:15 -0300

    Carregada imagem de exemplo com Picasso (pode ser imagem local)

--- app/src/main/java/com/rasfalto/SecondActivity.java
+++ app/src/main/java/com/rasfalto/SecondActivity.java
@@ -1,7 +1,8 @@
 package com.rasfalto;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,6 +11,10 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ImageView imagem = (ImageView) findViewById(R.id.image);
+        String url = "http://i.imgur.com/DvpvklR.png";
+        Picasso.with(this).load(url).into(imagem);
     }
 
     @Override
