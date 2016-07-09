ff2825137d41ba1f4ebf2945efcd28a345660216
RenanOfugi <renangyn2010@hotmail.com>
2016-07-09 03:05:04 -0300

    Preparando layout para suportar multiplos logins

---
+++ app/src/main/java/com/rasfalto/ChooseLoginActivity.java
@@ -0,0 +1,7 @@
+package com.rasfalto;
+
+/**
+ * Created by Renan O on 09/07/2016.
+ */
+public class ChooseLoginActivity {
+}
--- app/src/main/java/com/rasfalto/CreateAccountActivity.java
+++ app/src/main/java/com/rasfalto/CreateAccountActivity.java
@@ -8,15 +8,11 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Map;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
