0360c8ff3c92fa0847e67939f2a16ca30187dd93
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-07-16 01:07:41 -0300

    Escrito Javadoc para AccountService

--- app/src/main/java/com/rasfalto/service/AccountService.java
+++ app/src/main/java/com/rasfalto/service/AccountService.java
@@ -9,10 +9,28 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.rasfalto.CreateAccountActivity;
 
+/**
+ * Classe que reúne todas as operações de baixo nível relacionadas à gerência
+ * de contas de usuário no RAsfalto.
+ *
+ * @author gustavosotnas
+ * @author RenanOfugi
+ */
 public final class AccountService {
 
     private static boolean result;
 
+    /**
+     * Função que uma nova conta de usuário no RAsfalto, se comunicando com o
+     * Firebase para isso.
+     *
+     * @param email endereço de email do novo usuário.
+     * @param password a senha do novo usuário.
+     * @param mAuth o objeto de autenticação e manipulação do repositório no Firebase.
+     * @param targetActivity a Activity-alvo da operação.
+     * @return {@code true} caso a conta tenha sido criada com sucesso,
+     *         {@code false} caso contrário.
+     */
     public static boolean createAccount(String email, String password, FirebaseAuth mAuth, final CreateAccountActivity targetActivity) {
 
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(targetActivity, new OnCompleteListener<AuthResult>() {
