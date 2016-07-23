26228ca5e289eff566964bec710427fdbe026fad
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-07-22 21:53:49 -0300

    Testes com o aplicativo feitas

--- app/src/main/java/com/rasfalto/RAsfaltoActivity.java
+++ app/src/main/java/com/rasfalto/RAsfaltoActivity.java
@@ -1,13 +1,20 @@
 package com.rasfalto;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class RAsfaltoActivity extends AppCompatActivity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_rasfalto);
+    }
+
+    public void onClick$viewBuraco(View view) {
+        Intent verDetalhesBuraco = new Intent(this, BuracoActivity.class);
+        startActivity(verDetalhesBuraco);
     }
 }
