3e925144edca4587d9764da10ad18d15476cb23d
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-07-25 19:45:44 -0300

    Adicionado botão "Ver detalhes"

--- app/src/main/java/com/rasfalto/service/LoginService.java
+++ app/src/main/java/com/rasfalto/service/LoginService.java
@@ -3,14 +3,12 @@ package com.rasfalto.service;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.rasfalto.CreateAccountActivity;
 import com.rasfalto.LoginActivity;
 import com.rasfalto.R;
 import com.rasfalto.RAsfaltoActivity;
