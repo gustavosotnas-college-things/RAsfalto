161af5cdffc23f2ea1322ed55dd1754022360297
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-07-24 21:51:00 -0300

    Restaurados arquivos de front-end feitos ontem

--- app/src/main/java/com/rasfalto/RAsfaltoActivity.java
+++ app/src/main/java/com/rasfalto/RAsfaltoActivity.java
@@ -1,7 +1,9 @@
 package com.rasfalto;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class RAsfaltoActivity extends AppCompatActivity
 {
@@ -9,5 +11,10 @@ public class RAsfaltoActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rasfalto);
+    }
+
+    public void onClick$viewBuraco(View view) {
+        Intent verDetalhesBuraco = new Intent(this, BuracoActivity.class);
+        startActivity(verDetalhesBuraco);
     }
 }
